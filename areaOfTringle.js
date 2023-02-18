cd71fbc38f4f3c902e9550e8e25d8d9ddf314a15
Saurav Kumar <thakursauravkumar015@gmail.com>
2023-02-18 14:56:28 +0530

    Area of a tringlr

---
+++ areaOfTringle.js
@@ -0,0 +1,6 @@
+// Program to Calculate the Area of a Triangle
+function areaOfTringle(base,height){
+    return `Area of Tringle is :${(base*height)/2}`
+}
+const returnValue = areaOfTringle(5,10)
+console.log(returnValue)
