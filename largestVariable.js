5c91fdf5723f1009c11a6f074a143f62f48acd86
Saurav Kumar <thakursauravkumar015@gmail.com>
2023-02-18 19:55:07 +0530

    find the largest among given number

---
+++ largestVariable.js
@@ -0,0 +1,13 @@
+// Find the largest number among three numbers
+let a = 144
+let b = 118
+let c = 32
+
+if (a > b & b > c) {
+    console.log("a is greater")
+}
+else if (b > a & b > c) {
+    console.log("b is greater")
+}
+else
+console.log ("c is greater")
