eb4b96327218dc98a000a95f1b613ea2ac205310
Saurav Kumar <thakursauravkumar015@gmail.com>
2023-02-19 00:30:50 +0530

    Swap of two variable

---
+++ swapVariable.js
@@ -0,0 +1,11 @@
+// Program to Swap Two Variables values
+
+let a = 1;
+let b = 2;
+
+function swapVariable(a,b){
+    return [a , b] = [b , a]
+}
+
+const returnValue = swapVariable(140 , 180);
+console.log(returnValue)
