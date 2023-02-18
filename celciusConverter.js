2b544b7abdefe6bd7014be341181cf16218c6a06
Saurav Kumar <thakursauravkumar015@gmail.com>
2023-02-18 16:51:10 +0530

    Celcius to Fahrenheit Converter

---
+++ celciusConverter.js
@@ -0,0 +1,7 @@
+// Program to Convert Celsius to Fahrenheit
+function celciusToFahrenheit(input){
+        return (input*9/5)+32;
+}
+
+const returnValue = celciusToFahrenheit(9)
+console.log(returnValue,"F");
